6ce9a4b14d5b81b14fc61af8777af6c029457c76
static <pony@simplel.de>
2017-04-19 14:29:55 +0200

    Found out how to fetch the archive. o.O

--- src/hdl.py
+++ src/hdl.py
@@ -1,6 +1,7 @@
 # Heise downloader (hdl)
 import os
 from urllib.request import urlopen
+import urllib.error
 from bs4 import BeautifulSoup
 from src import helpers
 
@@ -9,13 +10,19 @@ cwd = os.path.dirname(__file__)
 print(cwd)
 archive_path = os.path.join(cwd, '../archive/')
 print(archive_path)
-base_url = 'https://www.heise.de/'
-url = 'https://www.heise.de/newsticker'
+BASE_URL = 'https://www.heise.de/'
+# https://www.heise.de/newsticker/archiv/?jahr=2017;woche=3
+URL = 'https://www.heise.de/newsticker'
+ARCHIVE_BASE_URL = 'https://www.heise.de/newsticker/archiv/'
 
 
 def get_page(url):
-    response = urlopen(url)
-    webContent = response.read()
+    try:
+        response = urlopen(url)
+        webContent = response.read()
+    except urllib.error.HTTPError as e:
+        print(e.code)
+        return None
 
     return webContent
 
@@ -37,20 +44,41 @@ def extract_article_links(content):
 
 def get_articles(article_links):
     for article_id, href in article_links.items():
-        print("==== " + article_id + "====")
+        print("article id: " + article_id)
         if os.path.isfile(archive_path + article_id):
-            print("file exists")
+            print("file exists -> skipping")
             continue
-        soup = BeautifulSoup(get_page(base_url+href))
+        soup = BeautifulSoup(get_page(BASE_URL+href))
         with open(archive_path + article_id, 'w') as f:
             f.write(soup.prettify())
         for article in soup.find_all(attrs={"data-article-type": "meldung"}):
             print("article downloaded")
 
 
+def fetch_archive():
+    WEEKS = 52
+    YEARS = ['2017', '2016', '2015']
+
+    for year in YEARS:
+        for week in range(WEEKS):
+            archive_url = ARCHIVE_BASE_URL
+            archive_url += "?Jahr=" + year + ";woche=" + str(week)
+
+            extract_url = str(get_page(archive_url))
+
+            if extract_url:
+                links = extract_article_links(extract_url)
+                print("=== retrieving articles ["
+                      + year + " week " + str(week) + "] ===")
+                get_articles(links)
+
+    return 0
+
+
 def main():
-    links = extract_article_links(str(get_page(url)))
-    get_articles(links)
+    fetch_archive()
+    # links = extract_article_links(str(get_page(URL)))
+    # get_articles(links)
     # print(links)
 
 
