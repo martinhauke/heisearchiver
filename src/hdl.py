a4a3c9ea2c0f4c81142f78c5d546eab0bb1b90d8
PrettyPinkPonyOfDeath <martin.hauke.jr@gmail.com>
2017-04-20 08:16:47 +0200

    Added some checks and docstrings; Fixed week range

--- src/hdl.py
+++ src/hdl.py
@@ -7,16 +7,24 @@ from src import helpers
 
 
 cwd = os.path.dirname(__file__)
-print(cwd)
 ARCHIVE_PATH = os.path.join(cwd, '../archive/')
-print(ARCHIVE_PATH)
 BASE_URL = 'https://www.heise.de/'
-# https://www.heise.de/newsticker/archiv/?jahr=2017;woche=3
 URL = 'https://www.heise.de/newsticker'
+# https://www.heise.de/newsticker/archiv/?jahr=2017;woche=3
 ARCHIVE_BASE_URL = 'https://www.heise.de/newsticker/archiv/'
 
 
+def print_paths():
+    """Prints the current path settings"""
+    print("Current directory: " + cwd)
+    print("Path to local Archive: " + ARCHIVE_PATH)
+    print("Base URL: " + BASE_URL)
+    # print("URL for link extraction: " + URL)
+    print("URL to the archive: " + ARCHIVE_BASE_URL)
+
+
 def get_page(url):
+    """Gets the content of the specified URL"""
     try:
         response = urlopen(url)
         webContent = response.read()
@@ -29,30 +37,32 @@ def get_page(url):
 
 def extract_article_links(content):
     """Tries to exctract links to articles and returns them in a list"""
-    soup = BeautifulSoup(content, 'html.parser')
     article_links = {}
+    if content:
+        soup = BeautifulSoup(content, 'html.parser')
 
-    for link in soup.find_all('a'):
-        lhref = link.get('href')
-        if "/meldung/" in lhref:
-            article_id_starts_at = lhref.rfind('-') + 1
-            article_id_ends_at = lhref.rfind('.')
-            article_id = lhref[article_id_starts_at:article_id_ends_at]
-            if helpers.is_valid_article_id(article_id):
-                article_links[article_id] = lhref
+        for link in soup.find_all('a'):
+            lhref = link.get('href')
+            if "/meldung/" in lhref:
+                article_id_starts_at = lhref.rfind('-') + 1
+                article_id_ends_at = lhref.rfind('.')
+                article_id = lhref[article_id_starts_at:article_id_ends_at]
+                if helpers.is_valid_article_id(article_id):
+                    article_links[article_id] = lhref
 
     return article_links
 
 
 def get_articles(article_links, path=ARCHIVE_PATH):
+    """Downloads and saves articles in an archive"""
     for article_id, href in article_links.items():
         print("article id: " + article_id + " link: " + href)
         if os.path.isfile(path + article_id):
-            print("file exists -> skipping")
+            print("*** file already exists -> skipping")
             continue
         content = get_page(BASE_URL+href)
         if not content:
-            print("Page not found")
+            print("[ERROR]: Page not found")
             continue
         soup = BeautifulSoup(content, "html.parser")
         with open(path + article_id, 'wb') as f:
@@ -69,7 +79,7 @@ def fetch_archive():
         archive_path = ARCHIVE_PATH + str(year) + "/"
         if not os.path.exists(archive_path):
             os.makedirs(archive_path)
-        for week in range(WEEKS):
+        for week in range(1, WEEKS + 1):
             archive_url = ARCHIVE_BASE_URL
             archive_url += "?jahr=" + str(year) + ";woche=" + str(week)
 
@@ -85,6 +95,7 @@ def fetch_archive():
 
 
 def main():
+    print_paths()
     fetch_archive()
     # links = extract_article_links(str(get_page(URL)))
     # get_articles(links)
