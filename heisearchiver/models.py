82cb68329af60ca4653e6d64e785296a52c09088
static <pony@simplel.de>
2017-05-03 12:34:09 +0200

    implemented all relation (i hope); added init and repr functions;

--- heisearchiver/models.py
+++ heisearchiver/models.py
@@ -42,26 +42,55 @@ image_source = Table('image_source', Base.metadata,
 
 
 # association table
-class Articles_Images(Base):
-    __tablename__ = 'articles_images'
+class Article_Image(Base):
+    __tablename__ = 'article_image'
     article_id = Column(Integer, ForeignKey('article.id'), primary_key=True)
     image_id = Column(Integer, ForeignKey('image.id'), primary_key=True)
-    alt = Column(String, nullable=True),
+    alt = Column(String, nullable=True)
     caption = Column(String, nullable=True)
 
-    Images = relationship("Image")
+    article = relationship("Article",
+                           backref=backref("article_image",
+                                           cascade="all, delete-orphan"))
+    image = relationship("Image",
+                         backref=backref('article_image',
+                                         cascade="all, delete-orphan"))
+
+    def __init__(self, article=None, image=None, alt=None, caption=None):
+        self.article = article
+        self.image = image
+        self.alt = alt
+        self.caption = caption
+
+    def __repr__(self):
+        return '<Article_Image {}>'.format(self.article.id + " "
+                                           + self.image.src)
 
 
 # association table
-class Articles_Links(Base):
-    __tablename__ = 'articles_links'
+class Article_Link(Base):
+    __tablename__ = 'article_link'
     article_id = Column(Integer, ForeignKey('article.id'), primary_key=True)
     link_id = Column(Integer, ForeignKey('link.id'), primary_key=True)
     rel = Column(String, nullable=True),
     text = Column(String),
     target = Column(String)
 
-    links = relationship("Link")
+    article = relationship("Article",
+                           backref=backref("article_link",
+                                           cascade="all, delete-orphan"))
+    link = relationship("Link", backref=backref("article_link",
+                                                cascade="all, delete-orphan"))
+
+    def __init__(self, article=None, link=None, text=None, rel=None):
+        self.article = article
+        self.link = link
+        self.text = text
+        self.rel = rel
+
+    def __repr__(self):
+        return '<Article_Link {}>'.format(self.article.id + " "
+                                          + self.link.href)
 
 
 class Article(Base):
@@ -99,11 +128,23 @@ class Article(Base):
                            backref=backref('article',
                                            cascade="all, delete-orphans"))
 
-    images = relationship("Article_Image")
-    links = relationship("Article_Link")
+    images = relationship("Article_Image", secondary='article_image',
+                          viewonly=True)
+    links = relationship("Article_Link", secondary='article_link',
+                         viewonly=True)
 
     subheadings = relationship("Subheading", backref='article')
     paragraphs = relationship("Paragraph", backref='article')
+
+    def __init__(self, article_id, meta_date=None, meta_description=None,
+                 meta_fulltitle=None):
+        self.id = article_id
+        self.meta_date = meta_date
+        self.meta_description = meta_description
+        self.meta_fulltitle = meta_fulltitle
+
+    def __repr__(self):
+        return '<Article {}>'.format(self.id)
 
 
 class Author(Base):
@@ -114,6 +155,14 @@ class Author(Base):
     name = Column(String(255))
     abbreviation = Column(String(10))
 
+    def __init__(self, name, abbreviation=None):
+        self.name = name
+        self.abbreviation = abbreviation
+
+    def __repr__(self):
+        return '<Author {}>'.format(self.name + " (" + self.abbriviation
+                                    + " )")
+
 
 class Topic(Base):
     __tablename__ = 'topic'
@@ -121,6 +170,12 @@ class Topic(Base):
     id = Column(Integer, primary_key=True)
 
     name = Column(String(64))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Topic {}>'.format(self.name)
 
 
 class Keyword(Base):
@@ -130,6 +185,12 @@ class Keyword(Base):
 
     name = Column(String(64))
 
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Keyword {}>'.format(self.name)
+
 
 class Source(Base):
     __tablename__ = 'source'
@@ -137,6 +198,16 @@ class Source(Base):
     id = Column(Integer, primary_key=True)
 
     name = Column(String(64))
+
+    images = relationship("Image", secondary=image_source,
+                          backref=backref('source',
+                                          cascade="all, delete-orphan"))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Source {}>'.format(self.name)
 
 
 class Image(Base):
@@ -146,6 +217,19 @@ class Image(Base):
 
     src = Column(String)
 
+    articles = relationship("Article", secondary='article_image',
+                            viewonly=True)
+
+    sources = relationship("Source", secondary=image_source,
+                           backref=backref('image',
+                                           cascade="all, delete-orphan"))
+
+    def __init__(self, src):
+        self.src = src
+
+    def __repr__(self):
+        return '<Image {}>'.format(self.src)
+
 
 class Link(Base):
     __tablename__ = 'link'
@@ -153,6 +237,15 @@ class Link(Base):
     id = Column(Integer, primary_key=True)
 
     url = Column(String)
+
+    articles = relationship("Article", secondary="article_link", viewonly=True)
+
+    def __init__(self, url):
+        self.url = url
+        self.articles = []
+
+    def __repr__(self):
+        return '<Link {}>'.format(self.url)
 
 
 class Subheading(Base):
@@ -163,6 +256,12 @@ class Subheading(Base):
     article_id = Column(ForeignKey('article.id'))
     text = Column(String)
 
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return '<Subheading {}>'.format(self.text)
+
 
 class Paragraph(Base):
     __tablename__ = 'paragraph'
@@ -172,3 +271,10 @@ class Paragraph(Base):
     article_id = Column(ForeignKey('article.id'))
     text_html = Column(String)
     text_plain = Column(String)
+
+    def __init__(self, text_html, text_plain):
+        self.text_html = text_html
+        self.text_plain = text_plain
+
+    def __repr__(self):
+        return '<Paragraph {}>'.format(self.id)
